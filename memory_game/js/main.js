95dedd9300282bbc900a475975b34d14a7aa0adc
Brandon <brandonblack02@gmail.com>
2019-08-20 20:03:40 -0700

    Objects to replace old arrays

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queenofhearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king", 
+	suit: "diamonds", 
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -18,9 +39,12 @@ if (cardsInPlay.length === 2) {
 		alert("Sorry try again");
 	}
 }
-cardsInPlay.push(cards[cardId]);
-console.log("User Flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
+console.log("User Flipped " + cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
 checkForMatch();
 }
+flipCard(1);
 flipCard(3);
 
